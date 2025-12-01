f22f61fed0534964533bd083b88f8d0d2a4138b1
Rahafsalman <144127007+RAHAFSALMAN7@users.noreply.github.com>
2025-12-01 11:12:51 +0200

    Delete assignment1 directory

--- assignment1/executor/JobStrategy.java
+++
@@ -1,8 +0,0 @@
-package edu.najah.cap.advance.assignments.assignment1.executor;
-
-import edu.najah.cap.advance.assignments.assignment1.connections.Connection;
-import edu.najah.cap.advance.assignments.assignment1.job.Job;
-
-public interface JobStrategy {
-    void execute(Job job, Connection conn);
-}
--- assignment1/templates/JobPrototype.java
+++
@@ -1,8 +0,0 @@
-package edu.najah.cap.advance.assignments.assignment1.templates;
-
-import edu.najah.cap.advance.assignments.assignment1.job.Job;
-
-public interface JobPrototype {
-    JobPrototype copy();
-    Job createJobInstance();
-}
